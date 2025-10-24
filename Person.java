1486b398a95b573d2b22380d7b69e0ce383b5532
CamB7 <cameronboyer1999@gmail.com>
2025-10-24 15:09:42 -0230

    -Person.java, Patient.java, and Doctor.java complete

---
+++ Person.java
@@ -0,0 +1,52 @@
+public abstract class Person {
+    // Instance variables
+    protected String id;
+    protected String name;
+    protected int age;
+    protected String phoneNumber;
+
+    public Person(String id, String name, int age, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    // toString method
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Age: " + age + ", Phone Number: " + phoneNumber;
+    }
+}
